10ebf94005a46aa1894e24f0a468e325dcf871b4
shoniblue <shoniblue@users.noreply.github.com>
2016-09-21 13:25:02 -0700

    Add files via upload

---
+++ src/main/java/com/sblue/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.sblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	
+    @RequestMapping(path="/greeting", method=RequestMethod.GET)
+    public String greeting() {
+        return new String("Hello World");
+    }
+}
